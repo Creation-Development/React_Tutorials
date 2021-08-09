aa2de537349590853660e13a7b3e05aa89e9b6e5
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2021-08-09 20:38:49 +0530

    add table to app

--- src/componets/ClassState.js
+++ src/componets/ClassState.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import { Table } from 'react-bootstrap'
 
 export default class ClassState extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            name :"",
-            email:"",
-            phone:"",
-            pass:""
+            name: "",
+            email: "",
+            phone: "",
+            pass: ""
         }
     }
     render() {
@@ -34,15 +35,58 @@ export default class ClassState extends Component {
                             <label htmlFor="pass" className="form-label">Password</label>
                             <input type="password" className="form-control" onChange={(e) => this.setState({ pass: e.target.value })} id="pass" />
                         </div>
-                        <button type="submit" onClick={(e)=> show(e,this.state)} className="btn btn-success w-100 my-4">Submit</button>
+                        <button type="submit" onClick={(e) => validation(e, this.state)} className="btn btn-success w-100 my-4">Submit</button>
                     </form>
                 </div>
+                <Table className="container my-4 text-center" striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Full Name</th>
+                            <th>E-Mail</th>
+                            <th>Mobile Number</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>{this.state.name}</td>
+                            <td>{this.state.email}</td>
+                            <td>{this.state.phone}</td>
+                            <td>{this.state.pass}</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </>
         )
     }
 }
 
-function show(e,data) {
+var validation = (e, data) => {
     e.preventDefault()
-    console.log(data);
+    if (data.name.length <= 2) {
+        alert("name is too short...!!")
+    }
+    else if (!data.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+        alert("email is not valid...!!")
+    }
+    else if (!data.phone.match(/[0-9]{10}/)) {
+        alert("mobile number is invalid")
+    }
+    else if (data.pass.length <= 8) {
+        alert("Password is too short....!!")
+    }
+    else {
+        show(e, data)
+    }
+}
+
+function show(e, data) {
+    e.preventDefault()
+    console.log(`Name : ${data.name}`);
+    console.log(`E-Mail : ${data.email}`);
+    console.log(`Phone : ${data.phone}`);
+    console.log(`Password : ${data.pass}`);
+    alert("check the console for output..")
 }
--- src/componets/FunctionState.js
+++ src/componets/FunctionState.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
+import { Table } from 'react-bootstrap'
 
 const FunctionState = () => {
-    const [name , setName] = useState("")
-    const [email , setEmail] = useState("")
-    const [pass , setPass] = useState("")
-    const [phone , setPhone] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+    const [phone, setPhone] = useState("")
     return (
         <>
             <div>
                 <div className="text-center">
                     <h1 className="text-center my-4">Function State Form</h1>
                 </div>
-                <form className="container-sm my-4">
+                <form className="container-sm my-4" onSubmit={(e) => validation(e, name, email, pass, phone)}>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Full Name</label>
                         <input type="text" className="form-control" onChange={(e) => setName(e.target.value)} id="name" aria-describedby="emailHelp" />
@@ -28,19 +29,60 @@ const FunctionState = () => {
                         <label htmlFor="pass" className="form-label">Password</label>
                         <input type="password" className="form-control" onChange={(e) => setPass(e.target.value)} id="pass" />
                     </div>
-                    <button type="submit" onClick={(e) => show(e, name,email,pass,phone)} className="btn btn-success w-100 my-4">Submit</button>
+                    <button type="submit" onClick={(e) => validation(e, name, email, pass, phone)} className="btn btn-success w-100 my-4">Submit</button>
                 </form>
             </div>
+            <Table className="container my-4 text-center" striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Full Name</th>
+                            <th>E-Mail</th>
+                            <th>Mobile Number</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>{name}</td>
+                            <td>{email}</td>
+                            <td>{phone}</td>
+                            <td>{pass}</td>
+                        </tr>
+                    </tbody>
+                </Table>
         </>
     )
 }
 
 export default FunctionState;
 
-function show(e,name,email,pass,phone){
+
+var validation = (e, name, email, pass, phone) => {
+    e.preventDefault()
+    if (name.length <= 2) {
+        alert("name is too short...!!")
+    }
+    else if (!email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+        alert("email is not valid...!!")
+    }
+    else if(!phone.match(/[0-9]{10}/) ){
+        alert("mobile number is invalid")
+    }
+    else if (pass.length <= 8) {
+        alert("Password is too short....!!")
+    }
+    else{
+        show(e,name,email,pass,phone)
+    }
+}
+
+function show(e, name, email, pass, phone) {
     e.preventDefault()
     console.log(`Name : ${name}`);
     console.log(`E-Mail : ${email}`);
     console.log(`Phone : ${phone}`);
     console.log(`Password : ${[pass]}`);
+    alert("check the console for output..")
 }
